f5227362a580c1f1e7016f449a412b54a77d8402
JeremyEmondLapierre <emondlapierrejeremy@gmail.com>
2021-04-20 20:01:52 -0400

    Ajout de titres aux différents projets

--- themes/theme-jel/template-parts/content-galerie.php
+++ themes/theme-jel/template-parts/content-galerie.php
@@ -12,7 +12,7 @@
 <article class="flip-card">
   <div class="flip-card-inner">
     <div class="flip-card-front">
-	<?php the_post_thumbnail( 'thumbnail' );?>
+	<?php the_post_thumbnail( 'medium' );?>
   </div>
   <div class="flip-card-back">
       <h1><a href="<?php echo get_permalink() ?>"> <?php the_title(); ?></h1>
